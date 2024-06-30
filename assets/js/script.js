5958b62d0d5a71908c3ecd0cd9f9cc52421ecad0
narmijo-tj <toffee.jaguargmail.com>
2024-06-30 02:32:15 -0400

    elementos js alert y tooltip agregados. Se limpia el formulario despues de enviar. Se cumple con los 2 elementos js solicitados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
+// js Tooltip bootstrap
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//Js propio
+let formContacto = document.getElementById("form-contacto");
+let enviarFormulario = document.getElementById("btn-contacto");
+enviarFormulario.addEventListener('click', function(event) {
+    event.preventDefault();
+    alert("El mensaje fue enviado correctamente.");
+    formContacto.reset();
+})
